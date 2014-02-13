80c7426ff0ac7eb36d432789f7893b89296dd0eb
Nicolai Steffensen <nicolai.steffensen@gmail.com>
2014-02-13 09:28:22 +0100

    Working with maven in NetBeans

--- src/main/java/dk/mercantec/hot/java/vitreher/matador/main.java
+++ src/main/java/dk/mercantec/hot/java/vitreher/matador/main.java
@@ -4,4 +4,9 @@ package dk.mercantec.hot.java.vitreher.matador;
  * Created by BoFjord on 13-02-14.
  */
 public class main {
+    
+    public static void main(String[] s) {
+        
+        System.out.println("test");
+  }
 }
